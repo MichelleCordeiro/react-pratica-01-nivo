44881a80483adef8480a6cd2f3720ca0017af259
MichelleCordeiro <mic321@gmail.com>
2024-02-21 01:22:42 -0300

    feat: list, filters & pagination

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,71 @@
-import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react'
+import { FileDown, Filter, MoreHorizontal, Plus, Search, Tag } from 'lucide-react'
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
 import { Pagination } from './components/pagination'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+import { useState } from 'react'
+
+// https://transform.tools/json-to-typescript
+// copia o json de http://localhost:3333/tags?_page=1&_per_page=200 no site transform
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  
+  const urlFilter = searchParams.get('filter') ?? ''
+  const [filter, setFilter] = useState(urlFilter)
+  
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+  
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', urlFilter, page],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`
+      );
+      const data = await response.json()
+
+      // delay 2s p testar o cache do react query
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      return data
+    },
+    // não pisca mais os dados, só vai exibir o conteúdo depois q ele carregar
+    placeholderData: keepPreviousData,
+    staleTime: 1000 * 60 // a cada 60seg qdo requisitar de novo aquela url vai limpar o cache
+  })
+
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', '1')
+      params.set('filter', filter)
+
+      return params
+    })
+  }
+
+  if (isLoading) {
+    return null
+  }
+
   return (
     <div className='py-10 space-y-8'>
       <div>
@@ -24,10 +83,22 @@ export function App() {
         </div>
 
         <div className='flex items-center justify-between'>
-          <Input variant='filter'>
-            <Search className='size-3' />
-            <Control placeholder='Search tags...' />
-          </Input>
+          <div className='flex items-center gap-2'>
+            <Input variant='filter'>
+              <Search className='size-3' />
+
+              <Control
+                placeholder='Search tags...'
+                onChange={e => setFilter(e.target.value)}
+                value={filter}
+              />
+            </Input>
+
+            <Button onClick={handleFilter}>
+              <Filter className='size-3' />
+              Filter
+            </Button>
+          </div>
 
           <Button>
             <FileDown className='size-3' />
@@ -46,35 +117,36 @@ export function App() {
           </TableHeader>
 
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => {
+            {tagsResponse?.data.map(tag => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
-
                   <TableCell>
                     <div className='flex flex-col gap-0.5'>
-                      <span className='font-medium'>React</span>
-                      <span className='text-sm text-zinc-500'>
-                        d9d8549c-6fbb-4588-8409-5e695b71e0df
-                      </span>
+                      <span className='font-medium'>{tag.title}</span>
+                      <span className='text-xs text-zinc-500'>{tag.id}</span>
                     </div>
                   </TableCell>
-
-                  <TableCell className='text-zinc-200'>13 vide0(s)</TableCell>
-
+                  <TableCell className='text-zinc-300'>{tag.amountOfVideos} video(s)</TableCell>
                   <TableCell className='text-right'>
                     <Button size='icon'>
                       <MoreHorizontal className='size-4' />
                     </Button>
                   </TableCell>
                 </TableRow>
-              )
+              );
             })}
           </TableBody>
         </Table>
 
-        <Pagination />
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={page}
+          />
+        )}
       </main>
     </div>
-  )
+  );
 }
