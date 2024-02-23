97bc761bfe5be074ddf181ccbd632cfac7be2e35
MichelleCordeiro <mic321@gmail.com>
2024-02-22 22:12:25 -0300

    feat: create a new tag

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,13 +1,13 @@
-import { Check, X } from 'lucide-react'
+import { Check, Loader2, X } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from './ui/button'
 import * as Dialog from '@radix-ui/react-dialog'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 const createTagSchema = z.object({
-  title: z.string().min(3, { message: 'Minimum 3 characters.' }),
-  slug: z.string()
+  title: z.string().min(3, { message: 'Minimum 3 characters.' })
 })
 
 type CreateTagSchema = z.infer<typeof createTagSchema>
@@ -22,15 +22,41 @@ function getSlugFromString(input: string): string {
 }
 
 export function CreateTagForm() {
-  const { register, handleSubmit, watch } = useForm<CreateTagSchema>({
+  const queryClient = useQueryClient()
+  
+  const { register, handleSubmit, watch, formState } = useForm<CreateTagSchema>({
     resolver: zodResolver(createTagSchema)
   })
   
-  function createTag(data: CreateTagSchema) {
-    console.log(data)
+  const slug = watch('title') 
+    ? getSlugFromString(watch('title')) :
+    ''
+  
+  const { mutateAsync } = useMutation({
+    mutationFn: async ({ title }: CreateTagSchema) => {
+      // delay 2sec
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      await fetch('http://localhost:3333/tags', {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          slug,
+          amountOfVideos: 0
+        })
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['get-tags'],
+      })
+    }
+  })
+
+  async function createTag({ title }: CreateTagSchema) {
+    await mutateAsync({ title })
   }
 
-  const slug = watch('title') ? getSlugFromString(watch('title')) : ''
 
   return (
     <form onSubmit={handleSubmit(createTag)} className='w-full space-y-6'>
@@ -51,13 +77,15 @@ export function CreateTagForm() {
           Slug
         </label>
         <input
-          {...register('slug')}
           id='slug'
           type='text'
           readOnly
           value={slug}
           className='w-full border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 text-sm'
         />
+        {formState.errors?.title && (
+          <p className='text-sm text-red-400'>{formState.errors.title.message}</p>
+        )}
       </div>
 
       <div className='flex items-center justify-end gap-2'>
@@ -68,11 +96,19 @@ export function CreateTagForm() {
           </Button>
         </Dialog.Close>
 
-        <Button className='bg-teal-400 text-teal-950' type='submit'>
-          <Check className='size-3' />
+        <Button
+          disabled={formState.isSubmitting}
+          className='bg-teal-400 text-teal-950'
+          type='submit'
+        >
+          {formState.isSubmitting ? (
+            <Loader2 className='size-3 animate-spin' />
+          ) : (
+            <Check className='size-3' />
+          )}
           Save
         </Button>
       </div>
     </form>
-  )
+  );
 }
